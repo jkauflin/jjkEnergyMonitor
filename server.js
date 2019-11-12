c553ca90c23d29224fc011fe4c1d66e7df47c33c
John J Kauflin <johnkauflin@gmail.com>
2019-11-11 20:31:51 -0500

    Updating to run on Pi

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 /*==============================================================================
-(C) Copyright 2018,2019 John J Kauflin, All rights reserved. 
+(C) Copyright 2016,2019 John J Kauflin, All rights reserved. 
 -----------------------------------------------------------------------------
 DESCRIPTION: Main nodejs server to run the web and control functions for
-                the grow environment monitor
+                the solar energy monitor
 -----------------------------------------------------------------------------
 Modification History
-2018-01-06 JJK  Initial version
+2016-06-12 JJK  Initial version to test making http requests to send data
+                to emoncms
+2016-06-21 JJK  Test recursive loop to run every X seconds
+2016-06-22 JJK  HTTP GET to send data to emoncms working
+2016-06-24 JJK  Working on getting Serial Port to work reading data from
+                Arduino board (getting an error trying to open the port)
+2016-06-25 JJK  Worked out understanding the SerialPort library and function
+                Got the port open and got good data from the Arduino using
+                the readline parser.  Working on loop and http now.
+                *** All logic moved over - ready to turn it on !!!
+2016-06-26 JJK  Adjusted lowCutoff from 12.45 to 12.40 and high from 13.15 to 13.17
+2016-10-17 JJK  Modified to work with new setup of using the grid-tie inverter.
+                Take the arduino data on pvVolts and pvAmps, calculate
+                pvWatts, and pvWattsUsed and post to JJK emoncms
+2017-05-21 JJK  Checking operation
+2017-08-26 JJK  Get working again after upgrading OS to Debian 8.9, Node 4.8.4
+2017-08-27 JJK  Updated serialport notation and use of parser for Readline
+                Added cleanStr to remove the carriage return (13) from the
+                end of the data string
 2017-12-26 JJK  Got working as a systemd service on BBB after upgrading
                 to BBB Debian 9.3 and NodeJS 6.12
-2018-03-07 JJK  Moved board functions to boardFunctions
-2018-03-21 JJK  Working on moisture and water control
-2018-05-18 JJK  Modified to present and allow configuration updates from
-                the web page
+2018-07-06 JJK  Back to the BBB after increasing panels from 4 to 6
+2018-07-07 JJK  Got it working with the old formulas and logging metrics
+                to my website
+2018-07-19 JJK  Got an Arduino Mega (SunFounder) to run StandardFirmataPlus
+                and the sensors, and got logging to emoncms again
+2018-08-10 JJK  Modified to only send to emoncms between 6am and 8pm
+-----------------------------------------------------------------------------
 2019-09-08 JJK  Upgraded to Raspbian Buster and NodeJS v10
 2019-09-28 JJK  Re-implementing web display and updates to config values
+2019-11-11 JJK  Pulling in the newest NodeJS coding ideas and getting the
+                Energy Monitor working again using just a Pi
 =============================================================================*/
 
 // Read environment variables from the .env file
@@ -61,8 +84,9 @@ httpServer.listen(WEB_PORT,function() {
 });
 
 // Include the Arduino board functions
-var boardFunctions = require('./boardFunctions.js');
+//var boardFunctions = require('./boardFunctions.js');
 
+/*
 app.get('/GetValues', function (req, res, next) {
     res.send(JSON.stringify(boardFunctions.getStoreRec()));
 });
@@ -81,3 +105,4 @@ app.post('/Water', function (req, res, next) {
     boardFunctions.water(req.body);
     res.send('ok');
 });
+*/
