7f324f4b78b03fd1559a2e2cdda79d01f7cd1932
John J Kauflin <johnkauflin@gmail.com>
2021-03-07 08:23:03 -0500

    test

--- boardFunctions.js
+++ boardFunctions.js
@@ -234,7 +234,7 @@ function logMetric() {
     currWatts = currVoltage * currAmperage;
 
     // Construct the JSON structure and URL to send values to the emoncms web site
-    /*
+    
     metricJSON = "{" + "pvVolts:" + currVoltage.toFixed(2) +
         ",pvAmps:" + currAmperage.toFixed(2) +
         ",pvWatts:" + currWatts.toFixed(2) +
@@ -245,11 +245,12 @@ function logMetric() {
         ",weatherHumidity:" + weatherHumidity.toString() +
         ",weatherDateTime:" + weatherDateTime.toString() +
         "}";
-    */
+    /*
     metricJSON = "{" + "pvVolts:" + currVoltage.toFixed(2) +
         ",pvAmps:" + currAmperage.toFixed(2) +
         ",pvWatts:" + currWatts.toFixed(2) +
         "}";
+    */
     emoncmsUrl = EMONCMS_INPUT_URL + "&json=" + metricJSON;
     log("logMetric, metricJSON = "+metricJSON);
 
